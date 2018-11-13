82281d99c8f0bee573170679aa39e6658724a7f5
Krisf16 <kristian.folkvord@hotmail.com>
2018-11-13 07:35:22 +0100

    Nytt oppsett
    Vi må ta en skikkelig rens og snakke.

---
+++ Idunno/CreateUser.js
@@ -0,0 +1,7 @@
+function CreateUser(){
+    
+    //document.getElementById('Create_Modal').style.display='none';
+    //Lukker modulen sin
+    //Kan brukes til f.eks. når logg inn fungerer
+    console.log("Clicked on CreateUser");
+}
---
+++ Idunno/Login.js
@@ -0,0 +1,7 @@
+function Login(){
+    
+    //document.getElementById('Login_Modal').style.display='none';
+    //Lukker modulen sin
+    //Kan brukes til f.eks. når logg inn fungerer
+    console.log("Clicked on Login");
+}
