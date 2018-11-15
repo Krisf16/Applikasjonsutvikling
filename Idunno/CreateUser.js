5f5279da6ba40f4a8d32a21dcbae3803bcb1a53e
Iselr15 <42719563+Iselr15@users.noreply.github.com>
2018-11-15 12:35:45 +0100

    Delete CreateUser.js

--- Idunno/CreateUser.js
+++
@@ -1,7 +0,0 @@
-function CreateUser(){
-    
-    //document.getElementById('Create_Modal').style.display='none';
-    //Lukker modulen sin
-    //Kan brukes til f.eks. når logg inn fungerer
-    console.log("Clicked on CreateUser");
-}
